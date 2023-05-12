93317e35c1fa7208ff3da97d00363ba308f19974
mpfaraujo <mpfaraujo@gmail.com>
2023-05-12 20:17:40 -0300

    Update clausulaQuatro.tsx

--- app/components/tCompromisso/clausulaQuatro.tsx
+++ app/components/tCompromisso/clausulaQuatro.tsx
@@ -8,7 +8,7 @@ export default function ClausulaQuatro(props:PageProps) {
     return (
 <>
 <div className='mx-[4em]'>
-  <h1 className='text-lg mt-6 w-full'><b><u>CLÁUSILA QUARTA - DAS OBRIGAÇÕES DA INSTITUIÇÃO DE ENSINO</u></b></h1>
+  <h1 className='text-lg mt-6 w-full'><b><u>CLÁUSULA QUARTA - DAS OBRIGAÇÕES DA INSTITUIÇÃO DE ENSINO</u></b></h1>
     <ol className='text-sm mt-[1em]'>
       <li>Acompanhar e supervisionar o estágio, podendo a qualquer tempo, propor a interrupção do estágio pelo não cumprimento, por quaisquer das partes envolvidas, das Cláusulas estabelecidas no Convênio para Concessão de Estágio;</li>
       <li>Designar professor orientador;<br/>
