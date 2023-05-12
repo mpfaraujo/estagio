151485bc7bbe7d7a59aff6f4e0e23081c2e5a1f5
mpfaraujo <mpfaraujo@gmail.com>
2023-05-12 20:17:02 -0300

    Update clausulaDois.tsx

--- app/components/tCompromisso/clausulaDois.tsx
+++ app/components/tCompromisso/clausulaDois.tsx
@@ -2,7 +2,7 @@ export  default function ClausulaDois() {
     return (
 <>
 <div className='mx-[4em]'>
-    <h1 className='text-lg mt-6 w-5/6'><b><u>CLÁUSILA SEGUNDA - DAS OBRIGAÇÕES DA CONCEDENTE</u></b></h1>
+    <h1 className='text-lg mt-6 w-5/6'><b><u>CLÁUSULA SEGUNDA - DAS OBRIGAÇÕES DA CONCEDENTE</u></b></h1>
 <ol className='text-sm mt-[1em]'>
   <li>Conceder ao <b>ESTAGIÁRIO</b> um estágio dentro de sua linha de formação sem configuração do vínculo de emprego, representado pela oportunidade de, nas dependências da <b>CONCEDENTE</b>, receber treinamento específico e ao mesmo tempo completando e consolidando, na prática, os ensinamentos teóricos que recebe na <b>INSTITUIÇÃO DE ENSINO</b>.</li>
   <li>Designar um Supervisor de Estágio, com formação, ou experiência profissional, na área de conhecimento correlata ao curso do estagiário, para acompanhar as atividades desenvolvidas no campo de estágio.<br/>
