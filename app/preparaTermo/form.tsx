a68e59078cdfabcf0ed586e9ee11e9dc2550099f
mpfaraujo <mpfaraujo@gmail.com>
2023-05-12 14:33:55 -0300

    Update form.tsx

--- app/preparaTermo/form.tsx
+++ app/preparaTermo/form.tsx
@@ -38,18 +38,12 @@ function onSubmit(e){
 }
     return (
         <form className='grid grid-cols-2 gap-6'name='dados'>
-        {/* <select name='iEnsino' onChange={handleChange} value={formValues.iEnsino}><option>--</option>
-            {iEnsinos.map((iEnsino)=>(<option key={iEnsino.cnpj} value={iEnsino.ie}>{iEnsino.ie}</option>))}
-        </select> */}
         <select name='estagiario' onChange={handleChange} value={formValues.estagiario}><option>--</option>
             {estagiarios.map((estagiario)=>(<option key={estagiario.cpf} value={estagiario.cpf}>{estagiario.estagiario}</option>))}
         </select>
         <select name='responsavelIE'onChange={handleChange} value={formValues.responsavelIE}><option>--</option>
             {responsaveisIE.map((responsavelIE)=>(<option key={responsavelIE.cpf} value={responsavelIE.cpf}>{responsavelIE.nome}</option>))}
         </select>
-        {/* <select name='cedente' onChange={handleChange} value={formValues.cedente}><option>--</option>
-            {responsaveisCefet.map((cedente)=>(<option key={cedente.cpf} value={cedente.cpf}>{cedente.nome}</option>))}
-        </select> */}
         <Link href={`/gerar/${formValues.estagiario}/${formValues.responsavelIE}`}>Gerar Termo de Compromisso</Link>
     </form>
     );
