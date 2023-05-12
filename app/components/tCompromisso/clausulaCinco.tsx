68ce5b673d8863df57a227d406056deb7cdbe074
mpfaraujo <mpfaraujo@gmail.com>
2023-05-12 20:18:05 -0300

    Update clausulaCinco.tsx

--- app/components/tCompromisso/clausulaCinco.tsx
+++ app/components/tCompromisso/clausulaCinco.tsx
@@ -2,7 +2,7 @@ export default function ClausulaCinco() {
     return (
 <>
 <div className='mx-[4em]'>
-  <h1 className='text-lg mt-6 w-full'><b><u>CLÁUSILA QUINTA - DISPOSIÇÕES GERAIS</u></b></h1>
+  <h1 className='text-lg mt-6 w-full'><b><u>CLÁUSULA QUINTA - DISPOSIÇÕES GERAIS</u></b></h1>
     <ol className='text-sm mt-[1em]'>
         <li>O Estágio será desenvolvido na <b>CONCEDENTE</b>, durante o período de <span contentEditable className='text-indigo-700 print:text-black'>DD/MM/AAA</span>  a <span contentEditable className='text-indigo-700 print:text-black'>DD/MM/AAA</span>, no horário de <b>07 h às 18 h 10 min</b>, sendo respeitado o limite máximo de 06 (seis) horas diárias, totalizando 30 (trinta) horas semanais, no máximo.</li>
         <li>Qualquer mudança em relação ao horário estabelecido no item “a” desta Cláusula, deverá ser resultado de prévio entendimento entre a <b>INSTITUIÇÃO DE ENSINO</b>, a <b>CONCEDENTE</b>, e o <b>ESTAGIÁRIO</b>, devendo ser compatível com as atividades escolares.</li>
