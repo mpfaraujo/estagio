09fde23999b6ffcc84ff3f94334dc84af266e635
mpfaraujo <mpfaraujo@gmail.com>
2023-05-12 20:16:39 -0300

    Update clausulaUm.tsx

--- app/components/tCompromisso/clausulaUm.tsx
+++ app/components/tCompromisso/clausulaUm.tsx
@@ -8,7 +8,7 @@ export default function ClausulaUm(props:PageProps) {
     return (
 <>
 <div className='mx-[4em] pt-[2em]'>
-    <h1 className='text-lg mt-[1em] w-5/6'><b><u>CLÁUSILA PRIMEIRA - DO OBJETO</u></b></h1>
+    <h1 className='text-lg mt-[1em] w-5/6'><b><u>CLÁUSULA PRIMEIRA - DO OBJETO</u></b></h1>
   <p className='text-sm mt-[1em]'>O presente termo tem por objetivo a concessão de estágio curricular ao estudante devidamente matriculado no âmbito da <b>INSTITUIÇÃO DE ENSINO</b>, de acordo com o que ficou estabelecido no Convênio assinado em {dataConvenio}, entre a <b>CONCEDENTE</b> e a <b>INSTITUIÇÃO DE ENSINO.</b></p>
   </div>
 </>
